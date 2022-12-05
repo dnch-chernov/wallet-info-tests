71bd82ded7b6e0eb0d129de1ad34b332e8fcc0a7
Denis Chernov <gitlab@dnch.me>
2022-12-04 23:15:47 -0800

    second test + report

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      inlineAssets: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress-tests/cypress/e2e/spec.cy.js
+++ cypress-tests/cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('happy path', () => {
+  it('eth address', () => {
     cy.visit('/')
+    cy.get('div[data-testid=dropdown-token]').click()
+    cy.get('div.item:nth-child(1)').click()
+    cy.get('div[data-testid=input-address] input').type(Cypress.env('eth_address'))
+    cy.get('button[data-testid=button-find]').click()
+    cy.get('h2[data-testid=header-value]').should("have.text", `${Cypress.env('eth_value')} ETH`)
+  })
+  it('usdc address', () => {
+    cy.visit('/')
+    cy.get('div[data-testid=dropdown-token]').click()
+    cy.get('div.item:nth-child(2)').click()
+    cy.get('div[data-testid=input-address] input').type(Cypress.env('usdc_address'))
+    cy.get('button[data-testid=button-find]').click()
+    cy.get('h2[data-testid=header-value]').should("have.text", `${Cypress.env('usdc_value')} USDC`)
   })
 })
