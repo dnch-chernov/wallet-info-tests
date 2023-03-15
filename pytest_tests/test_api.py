9974557b2f5026388a966699c20a712ad74e2844
Denis Chernov <gitlab@dnch.me>
2023-03-14 21:59:02 -0700

    fixed /status endpoint API test, updated readme

--- pytest_tests/test_api.py
+++ pytest_tests/test_api.py
@@ -2,7 +2,9 @@
 API tests
 """
 
+import os
 import pytest
+
 
 class TestStatus:
     """
@@ -14,21 +16,26 @@ class TestStatus:
         Happy path test.
         """
         client.get("/api/status")
-        print(f"\n ~~> time: {client.response.elapsed.total_seconds()}")
-        client.verify(json={"status":"Still alive!"})
+        client.verify(json={"connected": True})
+
 
 class TestBalance:
     """
     Tests for /api/balance endpoint.
     """
 
-    @pytest.mark.parametrize("token, value", [
-        ("eth", 0.024925767330357063),
-        ("usdc", 61.280028)
-    ])
+    wallet_address = os.getenv(
+        "WALLET_ADDRESS", "0x300045c41b5334772C25196ac0035bCDD511a821"
+    )
+    expected_eth_value = os.getenv("EXPECTED_ETH_VALUE", "0.024925767330357063")
+    expected_usdc_value = os.getenv("EXPECTED_USDC_VALUE", "61.280028")
+
+    @pytest.mark.parametrize(
+        "token, value", [("eth", expected_eth_value), ("usdc", expected_usdc_value)]
+    )
     def test_balance_eth_positive(self, client, token, value):
         """
         Happy path tests for /api/balance endpoint.
         """
-        client.get(f"/api/balance/{token}/0x300045c41b5334772C25196ac0035bCDD511a821")
+        client.get(f"/api/balance/{token}/{self.wallet_address}")
         client.verify(json={"crypto": token, "balance": value})
